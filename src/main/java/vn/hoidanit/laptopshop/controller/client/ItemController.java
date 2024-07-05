d091face631863095f8d5a8ef465ae74035f4dd6
Hoantp <tranphuchoan903@gmail.com>
2024-07-05 08:26:50 +0700

    5/7 detail product from client

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -5,12 +5,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import vn.hoidanit.laptopshop.service.ProductService;
 
 @Controller
 public class ItemController {
+    private final ProductService productService;
+
+    public ItemController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/product/{id}")
     public String getProductDetailPage(Model model, @PathVariable long id) {
-       return "/client/product/detail";
+        model.addAttribute("product", this.productService.getProductById(id).orElse(null));
+        return "/client/product/detail";
     }
-    
+
 }
